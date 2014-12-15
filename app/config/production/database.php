388b190320272b62f23da8e8da2e51f25339e5fe
Claire Yixie Li <yixie.li1985@gmail.com>
2014-12-14 20:35:09 -0800

    Added Database.php in production

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'pawbook',
+            'username'  => 'root',
+            'password'  => 'g3dHEWPve2',
+        ),
+
+    ),
+
+);
